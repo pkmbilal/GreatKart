ad8c85a8e1d75e548c9da0fd9cb1646b93f206e5
Mohammed Bilal <bilal00717@gmail.com>
2023-11-12 10:44:48 +0300

    User registration, email token based activation, user login added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -137,3 +137,18 @@ STATICFILES_DIRS = [
 # Media files configurations
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger"
+}
+
+
+# SMTP Configurations
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bilal00717@gmail.com'
+EMAIL_HOST_PASSWORD = 'kduvijgjxqhoelmg'
+EMAIL_USE_TLS = True
